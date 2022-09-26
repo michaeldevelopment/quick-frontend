dbb6f93ae2b5e1c593c9d154bc233cdd5c3ff0ec
Michael Sanabria Martinez <michael.sanabria@globant.com>
2022-09-26 16:06:26 -0500

    Applying SRP (1) SOLID Principle and clean code

--- src/Components/AllRecipes.js
+++ src/Components/AllRecipes.js
@@ -2,9 +2,7 @@ import React, { Suspense } from "react";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-
-import Spinner from "react-bootstrap/Spinner";
-const Recipe = React.lazy(() => import("../Components/Recipe"));
+import Recipes from "./Recipes";
 
 export default function AllRecipes({ recipes }) {
   return (
@@ -13,75 +11,13 @@ export default function AllRecipes({ recipes }) {
         <Container className="text-center my-4">
           <h3> Recetas Premium </h3>
           <Row>
-            {recipes.length ? (
-              recipes
-                .filter((recipe) => recipe.premium === true)
-                .map((recipe) => (
-                  <Suspense
-                    fallback={
-                      <Spinner
-                        animation="border"
-                        variant="secondary"
-                        className="mx-3"
-                      />
-                    }
-                    key={recipe.id}
-                  >
-                    <Recipe
-                      title={recipe.title}
-                      username={recipe.user.username}
-                      category={recipe.category}
-                      food_hour={recipe.food_hour}
-                      ingredients={recipe.ingredients}
-                      description={recipe.description}
-                      premium={recipe.premium}
-                      idRecipe={recipe.id}
-                      img={recipe.photos}
-                      date={recipe.createdAt}
-                      key={recipe.id}
-                    />
-                  </Suspense>
-                ))
-            ) : (
-              <p> No existen recetas por el momento </p>
-            )}
+            <Recipes recipes={recipes} />
           </Row>
         </Container>
         <Container className="text-center">
           <h3> Recetas Free </h3>
           <Row>
-            {recipes.length ? (
-              recipes
-                .filter((recipe) => recipe.premium === false)
-                .map((recipe) => (
-                  <Suspense
-                    fallback={
-                      <Spinner
-                        animation="border"
-                        variant="secondary"
-                        className="mx-3"
-                      />
-                    }
-                    key={recipe.id}
-                  >
-                    <Recipe
-                      title={recipe.title}
-                      username={recipe.user.username}
-                      category={recipe.category}
-                      food_hour={recipe.food_hour}
-                      ingredients={recipe.ingredients}
-                      description={recipe.description}
-                      idRecipe={recipe.id}
-                      premium={recipe.premium}
-                      img={recipe.photos}
-                      date={recipe.createdAt}
-                      key={recipe.id}
-                    />
-                  </Suspense>
-                ))
-            ) : (
-              <p> No existen recetas por el momento </p>
-            )}
+            <Recipes recipes={recipes} />
           </Row>
         </Container>
       </div>
